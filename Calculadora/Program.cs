5b4ccd2a55a77e2c87f1c542bd4a562a0ae43c65
FrankMSA <frankmsa@gmail.com>
2022-01-17 19:19:48 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,8 @@
+﻿using Calculadora;
+
+Console.WriteLine("Digite um número: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+int y = int.Parse(Console.ReadLine());
+
